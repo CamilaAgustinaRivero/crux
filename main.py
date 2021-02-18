afa400d5baf54ce9c7d738d40367cc12652571ba
ChrisErra <77129434+ChrisErra@users.noreply.github.com>
2021-02-17 22:20:57 -0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,14 +1,26 @@
 from chatterbot import ChatBot
 from chatterbot.trainers import ListTrainer
+from datetime import datetime
 
+def write_log(log,name,message):
+    string = str(message)
+    now = datetime.now()
+    date_and_time = now.strftime("%d/%m/%Y, %H:%M:%S")
+    line = ','.join([date_and_time,name,string]) + '\n'
+    log.write(line)
+    return log
 
 def get_conversation(chatbot):
     talk = True
+    log = open('log.txt','a')
     while talk:
         request = input('Usuario: ').lower()
+        write_log(log,'Usuario',request)
         response = chatbot.get_response(request)
         print('Crux: ', response)
+        write_log(log,'Crux',response)
         if 'chau' in request:
+            log.close()
             talk = False
 
 def main():
